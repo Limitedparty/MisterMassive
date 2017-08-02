416d740faa9445ae2ad2cdee2cd9beb233bea27d
Mark Vodyanitskiy <mvodya@gmail.com>
2017-08-02 19:56:45 +1000

    Готовая приличная версия v3
    Даже немного протестированная

--- MisterMassive/inputs.cpp
+++ MisterMassive/inputs.cpp
@@ -6,26 +6,26 @@ using namespace std;
 
 MM::actions MM::Action;
 
-// Массивы
+// Массивы (объявлены в классе)
 Massive MM::ary1;
 Massive MM::ary2;;
 
 void MM::StartInput() {
-	// Выводим возможные операции.
+	// Выводим возможные операции
 	cout << "Actions:\n(1) Summation massives\n(2) Multiplication massives\n(3) Transposition massive\n(4) Simple multiplication massive\n\n";
 
 	int now = 0;
 
-	// Заставляем выбирать до тех пор, пока нам не дадут существующую операцию.
-	while (StartInputValue(now)) { // См. функцию. Помимо проверки существования операции, она еще и присваивается (MM::actions).
+	// Заставляем выбирать до тех пор, пока нам не дадут существующую операцию
+	while (StartInputValue(now)) { // См. функцию. Помимо проверки существования операции, она еще и присваивается (MM::actions)
 		cout << "Select  action: ";
 		try
 		{
-			// Получаем ввод из клавиатуры и записываем в локальную переменную. Ее в свою очередь отправляем на проверку.
+			// Получаем ввод из клавиатуры и записываем в локальную переменную. Ее в свою очередь отправляем на проверку
 			cin >> now;
 		}
 		catch (exception e) {
-			// Выводим ошибочку, если нам втирают какую-то дичь.
+			// Выводим ошибочку, если нам втирают какую-то дичь
 			cout << "Error. Please enter integer value.";
 		}
 	}
@@ -34,7 +34,7 @@ void MM::StartInput() {
 void MM::MassiveSizeSet() {
 	int n, m;
 
-	// Вбиваем размеры первого массива.
+	// Вбиваем размеры первого массива
 	cout << "\nNumber of columns of the array #1: ";
 	cin >> m;
 	cout << "\nNumber of rows of the array #1: ";
@@ -42,11 +42,11 @@ void MM::MassiveSizeSet() {
 	// Выделяем память массиву
 	MM::ary1.init(n, m);
 
-	// Тут просто идет отскок, если операция требует всего один массив.
+	// Тут просто идет отскок, если операция требует всего один массив
 	if (MM::Action == mtransposition || MM::Action == smultiplication)
 		return;
 
-	// Вбиваем размеры второго массива.
+	// Вбиваем размеры второго массива
 	cout << "\nNumber of columns of the array #2: ";
 	cin >> m;
 	cout << "\nNumber of rows of the array #2: ";
@@ -68,8 +68,8 @@ void MM::MassiveInput() {
 		}
 	}
 
-	// Вбиваем данные одного массива, если так надо.
-	// Тут просто идет отскок, если операция требует всего один массив.
+	// Вбиваем данные одного массива, если так надо
+	// Тут просто идет отскок, если операция требует всего один массив
 	if (MM::Action == mtransposition || MM::Action == smultiplication)
 		return;
 
@@ -84,7 +84,7 @@ void MM::MassiveInput() {
 }
 
 void MM::MassiveSizeTest() {
-	 // TODO Проверка совместимости массивов.
+	 // TODO Проверка совместимости массивов
 }
 
 void MM::MassiveOperationStart() {
@@ -111,10 +111,10 @@ void MM::MassiveOperationStart() {
 	}
 }
 
-
+// Проверка валидности выбранной операции
 int MM::StartInputValue(int input) {
-	// В зависимости от выбранной цифры выбирает нужную нам операцию.
-	// Если прилетает какая-то дичь - возращаем 1 (т.е. true), что является сигналом об ошибке.
+	// В зависимости от выбранной цифры выбирает нужную нам операцию
+	// Если прилетает какая-то дичь - возращаем 1 (т.е. true), что является сигналом об ошибке
 	switch (input)
 	{
 	case 1:
