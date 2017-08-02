4ddb762c14f42b8c271ec72ab5d0cc940f5363eb
Mark Vodyanitskiy <mvodya@gmail.com>
2017-08-02 13:11:54 +1000

    Переписано сложение массивов

--- MisterMassive/massive.cpp
+++ MisterMassive/massive.cpp
@@ -1,10 +1,10 @@
 ﻿#include "massive.h"
 
-// Инициализация и создание массива.
-void Massive::init(int & m, int & n) {
+// Инициализация и создание массива
+void Massive::init(int & n, int & m) {
 	// Записываем размер массива
+	n_ = n;
 	m_ = m;
-	n_ = n; 
 
 	// Выделяем память массиву
 	data = new double *[m_];
@@ -13,20 +13,26 @@ void Massive::init(int & m, int & n) {
 	}
 }
 
-// Операция ввывода массива в консоль.
+// Операция вывода массива в консоль
 std::ostream & operator<<(std::ostream & os, const Massive & masive) {
 	// Читаем и выводим данные из массива
-	for (size_t m = 0; m < masive.m_; m++) {
-		for (size_t n = 0; n < masive.n_; n++) {
-			os << masive.data[m][n] << "   "; // Вывод с пробелом
+	for (size_t n = 0; n < masive.n_; n++) {
+		for (size_t m = 0; m < masive.m_; m++) {
+			os << masive.data[n][m] << "   "; // Вывод с пробелом
 		}
 		os << std::endl; // Переход на новую строку
 	}
 	return os;
 }
 
-// Сложение массивов.
+// Сложение массивов
 Massive Massive::operator+(Massive & massive) {
 	Massive operation;
-	return Massive();
+	operation.init(n_, m_); // Инициализируем локальный массив, который выкинем в ответ
+	for (int i = 0; i < n_; i++) { // Перебираем
+		for (int j = 0; j < m_; j++) { // весь массив
+			operation.data[m_][n_] = data[m_][n_] + massive.data[m_][n_]; // Собственно сама операция
+		}
+	}
+	return operation;
 }
--- MisterMassive/massive.h
+++ MisterMassive/massive.h
@@ -3,15 +3,17 @@
 
 class Massive {
 private:
-	double** data; // Собственно сам массив.
-	int m_, n_; // Размеры массива.
+	double** data; // Собственно сам массив
+	int n_, m_; // Размеры массива
 public:
-	void init(int & m, int & n); // Инициализация и создание массива.
-	friend std::ostream & operator<<(std::ostream & os, const Massive & masive); // Операция ввывода массива в консоль (с помощью cout).
+	void init(int & m, int & n); // Инициализация и создание массива
+	friend std::ostream & operator<<(std::ostream & os, const Massive & masive); // Операция ввывода массива в консоль (с помощью cout)
+	void set(int & m, int & n, double var); // Изменение элемента массива
+	void get(int & m, int & n); // Получение элемента массива
 
-	// Доступные операции.
-	Massive operator+(Massive & massive); // Сложение массивов.
-	Massive operator+(double & object); // Простое сложение массива.
-	Massive operator*(Massive & massive); // Умножение массивов.
-	Massive operator^(Massive & massive); // Транспонирование массивов.
+	// Доступные операции
+	Massive operator+(Massive & massive); // Сложение массивов
+	Massive operator+(double & var); // Простое сложение массива
+	Massive operator*(Massive & massive); // Умножение массивов
+	Massive operator^(Massive & massive); // Транспонирование массивов
 };
